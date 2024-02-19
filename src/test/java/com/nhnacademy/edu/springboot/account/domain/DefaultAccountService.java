d00fbcb16661261baa74eac77fd4f63740fb5dd2
halo-eunsu <ujd017@naver.com>
2024-02-19 13:08:39 +0900

    1

--- src/test/java/com/nhnacademy/edu/springboot/account/domain/DefaultAccountService.java
+++ src/test/java/com/nhnacademy/edu/springboot/account/domain/DefaultAccountService.java
@@ -1,4 +1,12 @@
 package com.nhnacademy.edu.springboot.account.domain;
 
+import com.nhnacademy.edu.springboot.account.repository.AccountRepository;
+
+import java.util.List;
+
 public class DefaultAccountService {
+
+    public AccountRepository accountRepository;
+
+    List<Account> getAccounts(){    return null;};
 }
